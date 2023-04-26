8bceb1f86df84337a0572dbd87725fdf0cd8beb7
KipronohVincent <kipronohvincent@gmail.com>
2023-04-26 19:48:47 +0300

    Sorting abstracted

--- sort_algos.cpp
+++ sort_algos.cpp
@@ -3,71 +3,74 @@
 #include <queue>
 using namespace std;
 
-// Selection sort with Priority Queue
-
-void selectionSortWithPQ(int arr[], int n) {
-    // Find the smallest element in the array
-    int smallest = arr[0];
-    for (int i = 1; i < n; i++) {
-        if (arr[i] < smallest) {
-            smallest = arr[i];
+class Sorter {
+public:
+    static void selectionSortWithPQ(int arr[], int n) {
+        int smallest = arr[0];
+        for (int i = 1; i < n; i++) {
+            if (arr[i] < smallest) {
+                smallest = arr[i];
+            }
+        }
+        std::priority_queue<int, std::deque<int>, std::greater<int>> pq;
+        for (int i = 0; i < n; i++) {
+            pq.push(arr[i]);
+        }
+        for (int i = 0; i < n; i++) {
+            arr[i] = pq.top();
+            pq.pop();
         }
     }
 
-    // Create a priority queue to hold the sorted elements
-    std::priority_queue<int, std::deque<int>, std::greater<int>> pq;
-
-    // Insert each element of the array into the priority queue
-    for (int i = 0; i < n; i++) {
-        pq.push(arr[i]);
-    }
-
-    // Extract the sorted elements from the priority queue
-    for (int i = 0; i < n; i++) {
-        arr[i] = pq.top();
-        pq.pop();
-    }
-}
-
-// Insertion sort with Priority Queue
-void insertionSortWithPQ(int arr[], int n) {
-    struct Element {
-        int value;
-        int index;
-        Element(int value, int index) : value(value), index(index) {}
-        bool operator>(const Element& other) const {
-            return value > other.value;
+    static void insertionSortWithPQ(int arr[], int n) {
+        struct Element {
+            int value;
+            int index;
+            Element(int value, int index) : value(value), index(index) {}
+            bool operator>(const Element& other) const {
+                return value > other.value;
+            }
+        };
+        std::priority_queue<Element, std::deque<Element>, std::greater<Element>> pq;
+        for (int i = 0; i < n; i++) {
+            pq.push(Element(arr[i], i));
         }
-    };
-
-    // Create a priority queue to hold the sorted elements
-    std::priority_queue<Element, std::deque<Element>, std::greater<Element>> pq;
-
-    // Insert each element of the array into the priority queue
-    for (int i = 0; i < n; i++) {
-        pq.push(Element(arr[i], i));
+        for (int i = 0; i < n; i++) {
+            Element e = pq.top();
+            pq.pop();
+            arr[i] = e.value;
+        }
     }
 
-    // Extract the sorted elements from the priority queue
-    for (int i = 0; i < n; i++) {
-        Element e = pq.top();
-        pq.pop();
-        arr[i] = e.value;
-    }
-}
-
-// Bubble sort
-void bubbleSort(int arr[], int n) {
-    for (int i = 0; i < n-1; i++) {
-        for (int j = 0; j < n-i-1; j++) {
-            if (arr[j] > arr[j+1]) {
-                int temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
+    static void bubbleSort(int arr[], int n) {
+        for (int i = 0; i < n-1; i++) {
+            for (int j = 0; j < n-i-1; j++) {
+                if (arr[j] > arr[j+1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
             }
         }
     }
-}
+
+    static void sort(int arr[], int n, int choice) {
+        switch(choice) {
+            case 1:
+                selectionSortWithPQ(arr, n);
+                break;
+            case 2:
+                insertionSortWithPQ(arr, n);
+                break;
+            case 3:
+                bubbleSort(arr, n);
+                break;
+            default:
+                cout << "Invalid choice.\n";
+                return;
+        }
+    }
+};
 
 int main() {
     int n, choice;
@@ -83,20 +86,7 @@ int main() {
     cout << "Choose sorting algorithm:\n1. Selection sort with Priority Queue\n2. Insertion sort with Priority Queue\n3. Bubble sort\n";
     cin >> choice;
 
-    switch(choice) {
-        case 1:
-            selectionSortWithPQ(arr, n);
-            break;
-        case 2:
-            insertionSortWithPQ(arr, n);
-            break;
-        case 3:
-            bubbleSort(arr, n);
-            break;
-        default:
-            cout << "Invalid choice.\n";
-            return 0;
-    }
+    Sorter::sort(arr, n, choice);
 
     ofstream outfile;
     outfile.open("sorted.txt");
